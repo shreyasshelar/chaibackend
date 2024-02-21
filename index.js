d9dbfc4290f6815ef361039b23a3b40ec3d984f1
shreyasshelar <shreyasshelarrr@gmail.com>
2024-02-21 21:31:54 +0530

    github data added

--- index.js
+++ index.js
@@ -9,6 +9,41 @@ const app = express()
 const port = 4000
 //when production may be another server with diff port so use .env for it
 
+const githubData={
+  "login": "shreyasshelar",
+  "id": 90932395,
+  "node_id": "MDQ6VXNlcjkwOTMyMzk1",
+  "avatar_url": "https://avatars.githubusercontent.com/u/90932395?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/shreyasshelar",
+  "html_url": "https://github.com/shreyasshelar",
+  "followers_url": "https://api.github.com/users/shreyasshelar/followers",
+  "following_url": "https://api.github.com/users/shreyasshelar/following{/other_user}",
+  "gists_url": "https://api.github.com/users/shreyasshelar/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/shreyasshelar/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/shreyasshelar/subscriptions",
+  "organizations_url": "https://api.github.com/users/shreyasshelar/orgs",
+  "repos_url": "https://api.github.com/users/shreyasshelar/repos",
+  "events_url": "https://api.github.com/users/shreyasshelar/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/shreyasshelar/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 2,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2021-09-17T19:19:36Z",
+  "updated_at": "2024-02-21T12:54:20Z"
+}
+
 
 //to serve 'get request' coming from client on / and always listen at / 
 //if request arrrived send response in terms of message to client 
@@ -27,6 +62,10 @@ app.get('/login',(req,res)=>{
 
 app.get('/youtube',(req,res)=>{
     res.send("<h2>chai aur code</h2>")
+})
+
+app.get('/github',(req,res)=>{
+  res.json(githubData)
 })
 
 
